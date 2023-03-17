cee5afa89f37fc855bf595fa6e2372ee7c848492
ncekumoyo <nhmoyo7@gmail.com>
2023-03-17 12:33:14 +0200

    save daily weights

--- src/components/weights/dynamicInputs.jsx
+++ src/components/weights/dynamicInputs.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export default function DynamicInputs({ name, sum, setSum }) {
   const [weights, setWeights] = useState([0]);
@@ -13,25 +13,20 @@ export default function DynamicInputs({ name, sum, setSum }) {
     const values = [...weights];
     values[index] = e.target.value;
     setWeights([...values]);
-  };
-
-  /* const handleFocus = (e) => {
-    if (e.target.value === "0") {
-      e.target.value = "";
-    }
-  }; */
-
-  useEffect(() => {
-    /* console.log(sum);
     setSum(() => {
       let total = 0;
-      for (let i = 0; i < weights.length; i++) {
-        total += Number(weights[i]);
+      for (let i = 0; i < values.length; i++) {
+        total += Number(values[i]);
       }
       return total;
     });
-    console.log(sum); */
-  }, []);
+  };
+
+  const handleFocus = (e) => {
+    if (e.target.value === "0") {
+      e.target.value = "";
+    }
+  };
 
   return (
     <div className="form-group mb-3">
@@ -46,6 +41,7 @@ export default function DynamicInputs({ name, sum, setSum }) {
             step="0.001"
             className="form-control form-control-sm me-2 diw "
             onChange={(e) => handleChange(index, e)}
+            onFocus={handleFocus}
             key={index}
             value={x}
           />
--- src/components/weights/weightsCreate.jsx
+++ src/components/weights/weightsCreate.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useFetch from "../../customhooks/useFetch";
 import DynamicInputs from "./dynamicInputs";
@@ -6,18 +6,30 @@ import DynamicInputs from "./dynamicInputs";
 export default function WeightsCreate() {
   const { data, doFetch } = useFetch("http://localhost:3002/weights");
   const [chuckSum, setChuckSum] = useState(0);
+  const [shortRibSum, setShortRibSum] = useState(0);
+  const [brisketSum, setBrisketSum] = useState(0);
+  const [boereworsSum, setBoereworsSum] = useState(0);
+  const [steakSum, setSteakSum] = useState(0);
+  const [chickenSum, setChickenSum] = useState(0);
   const navigate = useNavigate();
 
-  const handleSubmit = useCallback(
-    (e) => {
-      e.preventDefault();
-      doFetch({
-        method: "POST",
-        body: JSON.stringify({}),
-      });
-    },
-    [doFetch]
-  );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    doFetch({
+      method: "POST",
+      body: JSON.stringify({
+        date: new Date().toJSON().slice(0, 10),
+        meat: {
+          chuck: chuckSum,
+          shortrib: shortRibSum,
+          brisket: brisketSum,
+          steak: steakSum,
+          boerewors: boereworsSum,
+          chicken: chickenSum,
+        },
+      }),
+    });
+  };
 
   useEffect(() => {
     if (data) {
@@ -31,10 +43,29 @@ export default function WeightsCreate() {
         <h2>Weights - Enter for today</h2>
         <form onSubmit={handleSubmit} className="col-md-6">
           <DynamicInputs name="Chuck" sum={chuckSum} setSum={setChuckSum} />
-          <DynamicInputs name="Short Ribs" />
-          <DynamicInputs name="Brisket" />
-          <DynamicInputs name="Steak" />
-          <DynamicInputs name="Boerewors" />
+          <DynamicInputs name="Short Ribs" sum={shortRibSum} setSum={setShortRibSum} />
+          <DynamicInputs name="Brisket" sum={brisketSum} setSum={setBrisketSum} />
+          <DynamicInputs name="Steak" sum={steakSum} setSum={setSteakSum} />
+          <DynamicInputs name="Boerewors" sum={boereworsSum} setSum={setBoereworsSum} />
+          <div className="form-group mb-3">
+            <label className="fw-bold">Chicken {chickenSum}</label>
+            <div className="d-flex">
+              <input
+                type="number"
+                min="0"
+                step="1"
+                className="form-control form-control-sm me-2 diw "
+                onChange={(e) => {
+                  setChickenSum(Number(e.target.value));
+                }}
+                onFocus={(e) => {
+                  if (e.target.value === "0") {
+                    e.target.value = "";
+                  }
+                }}
+              />
+            </div>
+          </div>
           <div className="form-group">
             <button type="submit" className="btn btn-small btn-warning">
               Save
